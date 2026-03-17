6c2dee11a6ed58dd53f3f17dadff06c0acc98ef5
Admin <Admin@DESKTOP-OLC4IPP>
2026-03-18 00:57:57 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PROEL4W_MVC_Kaijenson_Motor_Shop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
